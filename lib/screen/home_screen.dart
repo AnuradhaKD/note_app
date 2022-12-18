e5308715a6746ab8585d07a4957d941546a52b98
D.M.Anuradha Kumara Dissanayake <d.m.a.k.dissanayake.1997@gmail.com>
2022-12-18 23:12:19 +0530

    update some files

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:note_app/style/style.dart';
+import 'package:note_app/widgets/note_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +14,66 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: const Text("First Note"),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your recent Notes",
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection("Notes").snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return GridView(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2),
+                      children: snapshot.data!.docs
+                          .map((note) => noteCard(() => {}, note))
+                          .toList(),
+                    );
+                  }
+                  return Text(
+                    "There's no Notes",
+                    style: GoogleFonts.nunito(color: Colors.white),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('Add Note'),
+        icon: const Icon(Icons.add),
+      ),
+    );
   }
 }
